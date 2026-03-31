59da85e410eb2b3d501f0c153df5180f2ea88095
Fa-ce <3051810772@qq.com>
2026-03-31 20:01:51 +0800

    feat: 新闻详情接口

--- crud/news.py
+++ crud/news.py
@@ -1,7 +1,7 @@
 # 新闻相关数据库操作
 
 from models.news import Category, News
-from sqlalchemy import func, select
+from sqlalchemy import func, select, update
 from sqlalchemy.ext.asyncio import AsyncSession
 
 
@@ -29,3 +29,52 @@ async def get_new_count(db: AsyncSession, category_id: int):
     stmt = select(func.count(News.id)).where(News.category_id == category_id)
     result = await db.execute(stmt)
     return result.scalar_one()  # 只能有一个结果，否则报错
+
+
+# 查询新闻详情
+async def get_news_detail(db: AsyncSession, news_id: int):
+    stmt = select(News).where(News.id == news_id)
+    result = await db.execute(stmt)
+    return result.scalar_one_or_none()
+
+
+# 增加浏览量
+async def increase_news_views(db: AsyncSession, news_id: int):
+    stmt = update(News).where(News.id == news_id).values(views=News.views + 1)
+    result = await db.execute(stmt)
+    await db.commit()
+    # 更新 → 检测数据库是否真的命中了数据 → 命中了返回 True
+    return result.rowcount > 0
+
+
+# 同类新闻推荐
+async def get_related_news(
+    db: AsyncSession, category_id: int, news_id: int, limit: int = 5
+):
+    stmt = (
+        select(News)
+        .where(News.category_id == category_id, News.id != news_id)
+        .order_by(
+            News.views.desc(),  # 默认升序，使用 desc() 降序
+            News.publish_time.desc(),  # 发布时间
+        )
+        .limit(limit)
+    )
+    result = await db.execute(stmt)
+    related_news = result.scalars().all()
+    # 列表推导式，推出新闻核心数据，返回新闻列表
+    return [
+        {
+            "id": news.id,
+            "title": news.title,
+            "content": news.content,
+            "image": news.image,
+            "author": news.author,
+            "publishTime": news.publish_time,
+            "categoryId": news.category_id,
+            "views": news.views,
+        }
+        for news in related_news
+    ]
+
+    # return result.scalars().all()

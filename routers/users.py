e63cf33845bd4531c183a8a44f310cf6a5b4b190
Fa-ce <historiographer@foxmail.com>
2026-04-18 19:53:39 +0800

    feat: 用户登录接口

--- routers/users.py
+++ routers/users.py
@@ -39,3 +39,29 @@ async def register(db: AsyncSession = Depends(get_db), user: UserRequest = None)
         token=token, user_info=UserInfoResponse.model_validate(new_user)
     )
     return response.success_response(message="注册成功", data=response_data)
+
+
+""" 
+登录
+请求方法： post
+参数： Pydantic 类型
+检查用户是否存在 → 不存在返回 None，存在验证用户和密码 → 生成token，返回响应结果
+"""
+
+
+# 用户登录
+@router.post("/login")
+async def login(
+    user_data: UserRequest,
+    db: AsyncSession = Depends(get_db),
+):
+    user = await users.authenticate_user(db, user_data.username, user_data.password)
+
+    if not user:
+        raise HTTPException(status_code=401, detail="用户不存在或密码错误")
+
+    token = await users.create_token(db, user.id)
+    response_data = UserAuthResponse(
+        token=token, user_info=UserInfoResponse.model_validate(user)
+    )
+    return response.success_response(message="登录成功", data=response_data)
